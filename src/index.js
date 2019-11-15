b1e5cf11ab2170bdf8a270c42d61d7de2307f13c
gboyega <jaiyeolagboyega@gmail.com>
2019-11-15 11:31:48 +0100

    created 404 page using react-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import App from './app'
+import Users from './user'
+import Notfound from "./Notfound"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/user">Users</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/user" component={Users} />
+                <Route component={Notfound} />
+            </Switch>
+            
+        </div>
+    </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(routing, document.getElementById('root'))
---
+++ src/user.js
@@ -0,0 +1,10 @@
+import React from 'react'
+class Users extends React.Component {
+    render() {
+        return <div>
+            <h1>Users</h1>
+            <p>This is the User page</p>
+        </div>
+    }
+}
+export default Users
